fbdc8526e3781d1877f18a7051863d935a27346d
Vaishnavi <vs2753@nyu.edu>
2024-10-01 18:55:13 -0400

    changes to fix await issues

--- main.py
+++ main.py
@@ -15,91 +15,71 @@ import time
 
 async def connect():
     opts = RobotClient.Options.with_api_key(
-            # Replace "<API-KEY>" (including brackets) with your machine's api key 
         api_key='i11ph4btwvdp1kixh3oveex92tmvdtx2',
-        # Replace "<API-KEY-ID>" (including brackets) with your machine's api key id
         api_key_id='8b19e462-949d-4cf3-9f7a-5ce0854eb7b8'
     )
     return await RobotClient.at_address('rover6-main.9883cqmu1w.viam.cloud', opts)
 
-async def getDetections(colorDetector,cam,vel):
+async def getDetections(colorDetector, cam, base, vel):
     detections = await colorDetector.get_detections_from_camera(cam)
     if not detections:
         for i in range(36):
-            base.spin(10,vel)
+            await base.spin(10, vel)
             detections = await colorDetector.get_detections_from_camera(cam)
             if detections:
                 break
-    if detections:
-        return detections
-    else:
+    return detections if detections else None
+
+def findRange(detections):
+    adequateConfidence = [d for d in detections if d.confidence > 0.5]
+    if not adequateConfidence:
         return None
-
-
-def findRange (detections):
-    adequateConfidence = []
-    for detection in detections:
-        if detection["confidence"]>0.5:
-            adequateConfidence.append(detection)
-    bestDetection = adequateConfidence[0]
-    bestArea = (bestDetection.x_max-bestDetection.x_min)*(bestDetection.y_max-bestDetection.y_min)
-    for d in adequateConfidence:
-        area = (d.x_max-d.x_min)*(d.y_max - d.y_min)
-        if area > bestArea:
-            bestArea = area
-            bestDetection =d
-    return d
+    bestDetection = max(adequateConfidence, key=lambda d: (d.x_max - d.x_min) * (d.y_max - d.y_min))
+    return bestDetection
 
 def leftOrRight(detection, midpoint):
     if detection:
-        detectionMP = (detection.x_min+detection.x_max)/2
-        difference = midpoint-detectionMP
+        detectionMP = (detection.x_min + detection.x_max) / 2
+        difference = midpoint - detectionMP
         return difference
     else:
         print("no detection available")
         return None
     
-def detectDistance(detection, dist, vel):
-    #declare a range for area
-    xspan = detection.x_max-detection.x_min
-    xspanMin=0.5 *xspan
-    xspanMax= xspan
-    #will the ranges modify (I haven't coded in python in so long Im so used to C)
-    #detect a 2d estimate for the size change of an object
-    #move it until it
-    if xspan<xspanMin:
-        while xspan<xspanMax:
-            base.move_straight(dist,vel)
+async def detectDistance(detection, base, dist, vel):
+    xspan = detection.x_max - detection.x_min
+    xspanMin = 0.5 * xspan
+    xspanMax = xspan
+    if xspan < xspanMin:
+        while xspan < xspanMax:
+            await base.move_straight(dist, vel)
+            # You might want to update the detection here to get the new xspan
 
-def motion(detection, dist, vel, mp):
+async def motion(detection, base, dist, vel, mp):
     while True:
         diff = leftOrRight(detection, mp)
-        base.spin(diff, vel)
-        detectDistance(detection, dist,vel)
-
+        if diff is not None:
+            await base.spin(diff, vel)
+        await detectDistance(detection, base, dist, vel)
+        await asyncio.sleep(0.1)  # Add a small delay to prevent tight looping
 
 async def main():
-    #following straight from tutorial, edit
     machine = await connect()
     camera_name = "cam"
     camera = Camera.from_robot(machine, camera_name)
-    frame = await camera.get_image(mime_type="image/jpeg")
     base = Base.from_robot(machine, "viam_base")
-    pil_frame = viam_to_pil_image(frame)
-    my_detector = VisionClient.from_robot(machine, "color_detector") #change name to match color detector
-    
+    my_detector = VisionClient.from_robot(machine, "color_detector")
 
-    #find detections
-    #then find range
-    #save detection
-    #then use a daemon thread to find if it is left or right, rotate that much in that direction, find distance, move forward that much
-    #press enter to quit
-    detections = getDetections(my_detector,camera,0.8)
+    detections = await getDetections(my_detector, camera_name, base, 0.8)
     detection = findRange(detections)
-    threading.Thread(target=motion, daemon=True).start()
-    input("Press enter to quit")
-    
-    # Don't forget to close the machine when you're done!
+
+    if detection:
+        asyncio.create_task(motion(detection, base, 10, 0.5, 0.5))  # Adjust parameters as needed
+        print("Motion task started. Press Enter to quit.")
+        await asyncio.get_event_loop().run_in_executor(None, input, "")
+    else:
+        print("No detections found.")
+
     await machine.close()
 
 if __name__ == '__main__':
