b185b8ae0db3869ab184f1c7e341ec40358734c7
Vaishnavi <vs2753@nyu.edu>
2024-10-01 01:34:26 -0400

    move test code to new file

--- main.py
+++ main.py
@@ -102,58 +102,6 @@ async def main():
     threading.Thread(target=motion, daemon=True).start()
     input("Press enter to quit")
     
-
-    
-
-    print('Resources:')
-    print(machine.resource_names)
-    
-    # Note that the pin supplied is a placeholder. Please change this to a valid pin you are using.
-    # local
-    local = Board.from_robot(machine, "local")
-    local_return_value = await local.gpio_pin_by_name("16")
-    print(f"local gpio_pin_by_name return value: {local_return_value}")
-
-    # right
-    right = Motor.from_robot(machine, "right")
-    right_return_value = await right.is_moving()
-    print(f"right is_moving return value: {right_return_value}")
-
-    # left
-    left = Motor.from_robot(machine, "left")
-    left_return_value = await left.is_moving()
-    print(f"left is_moving return value: {left_return_value}")
-
-    # viam_base
-    viam_base = Base.from_robot(machine, "viam_base")
-    viam_base_return_value = await viam_base.is_moving()
-    print(f"viam_base is_moving return value: {viam_base_return_value}")
-
-    # cam
-    cam = Camera.from_robot(machine, "cam")
-    cam_return_value = await cam.get_image()
-    print(f"cam get_image return value: {cam_return_value}")
-
-    # Renc
-    renc = Encoder.from_robot(machine, "Renc")
-    renc_return_value = await renc.get_position()
-    print(f"Renc get_position return value: {renc_return_value}")
-
-    # Lenc
-    lenc = Encoder.from_robot(machine, "Lenc")
-    lenc_return_value = await lenc.get_position()
-    print(f"Lenc get_position return value: {lenc_return_value}")
-
-    # accelerometer
-    accelerometer = MovementSensor.from_robot(machine, "accelerometer")
-    accelerometer_return_value = await accelerometer.get_linear_acceleration()
-    print(f"accelerometer get_linear_acceleration return value: {accelerometer_return_value}")
-
-    # color_detector
-    color_detector = VisionClient.from_robot(machine, "color_detector")
-    color_detector_return_value = await color_detector.get_properties()
-    print(f"color_detector get_properties return value: {color_detector_return_value}")
-
     # Don't forget to close the machine when you're done!
     await machine.close()
 
