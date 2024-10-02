5d1a2cd08d24c7bece54a8b4f6b245fb0825fa4c
Vaishnavi <vs2753@nyu.edu>
2024-10-02 19:16:21 -0400

    change spin and vel

--- main.py
+++ main.py
@@ -44,9 +44,9 @@ async def leftOrRight(detection, midpoint):
         difference = midpoint - detectionMP
         if difference == 0:
             return 0
-        elif difference <midpoint/4:
+        elif difference <midpoint/6:
             return -1
-        elif difference>midpoint/4:
+        elif difference>midpoint/6:
             return 1
         
             while abs(difference)>midpoint/6:
